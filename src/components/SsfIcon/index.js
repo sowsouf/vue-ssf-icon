83453c19d90c8ef56edec259916d8fe600136257
Sofiane Akbly <sofiane.akbly@gmail.com>
2020-01-06 14:08:40 +0100

    Set directory path

--- src/components/SsfIcon/index.js
+++ src/components/SsfIcon/index.js
@@ -1,4 +1,4 @@
-import SsfIcon            from "./components/Main";
+import SsfIcon            from "@/components/SsfIcon/components/Main";
 import SsfFontAwesomeIcon from "@/components/SsfIcon/components/includes/FontAwesomeIcon";
 import SsfLineAwesomeIcon from "@/components/SsfIcon/components/includes/LineAwesomeIcon";
 import SsfMaterialIcon    from "@/components/SsfIcon/components/includes/MaterialIcon";
